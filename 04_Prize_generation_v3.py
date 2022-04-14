85f8313d04773eb3656ba83e78b0e3d603cbb9a9
sonjall <101675148+sonjall@users.noreply.github.com>
2022-04-14 13:24:08 +1200

    Add files via upload

---
+++ 04_Prize_generation_v3.py
@@ -0,0 +1,43 @@
+# Increased trials and just showed stats
+
+import random
+
+NUM_TRIALS = 100
+winnings = 0
+
+cost = NUM_TRIALS * 5
+
+for item in range(0, NUM_TRIALS):
+    prize = ""
+    round_winnings = 0
+
+    for thing in range(0, 3):
+
+        # randint finds numbers between given endpoints, including both endpoints
+        prize_num = random.randint(1, 4)
+        # prize += " "
+        if prize_num == 1:
+            # one in ten change of getting gold
+            # prize += "gold"
+            round_winnings += 5
+        elif prize_num == 2:
+            # get silver if it's a 2 or 3
+            # prize += "silver"
+            round_winnings += 2
+        elif prize_num == 3:
+            # copper if it's 4, 5, 6, 7 <40% chance of copper>
+            # prize += "copper"
+            round_winnings += 1
+        else:
+            prize += "lead"
+
+    # print("You won {} which is worth {}".format(prize, round_winnings))
+    winnings += round_winnings
+
+print("Paid in ${}".format(cost))
+print("Pay Out: ${}".format(winnings))
+
+if winnings > cost:
+    print("You came out ${} ahead".format(winnings - cost))
+else:
+    print("Sorry, you lost ${}".format(cost - winnings))
